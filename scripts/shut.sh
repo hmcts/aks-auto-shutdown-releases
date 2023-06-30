f3b4ebcf0dab52568e61306b3a686690053fba7d
Jordan Hoey <jordan.hoey@justice.gov.uk>
2023-06-30 15:14:11 +0100

    test

--- scripts/shut.sh
+++ scripts/shut.sh
@@ -30,8 +30,8 @@ jq -c '.[]' <<< $SUBSCRIPTIONS | while read subscription; do
             ED=$(jq -r '."Skip shutdown end date"' <<< $id)
             echo $SD
             echo $ED
-            SDS=$(date -d "'"$SD"'" +"%s")
-            EDS=$(date -d "'"$ED"'" +"%s")
+            SDS=$(date -d 30-06-2023 +"%s")
+            EDS=$(date -d 01-07-2023 +"%s")
             TOSEC=$(date -d "'"$to_date"'" +"%s")
             echo $SDS
             echo $EDS
