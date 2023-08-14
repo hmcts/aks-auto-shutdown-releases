4647413ef1f6b1549b133a024faadd05a7db0695
Jordan Hoey <jordan.hoey@justice.gov.uk>
2023-08-14 12:06:06 +0100

    adding missing ')'

--- cost-calculator.py
+++ cost-calculator.py
@@ -58,7 +58,7 @@ def calculate_cost(env_rate, vm_num_int, skip_bus_days, skip_weekend_days):
     print("======")
 
     with open(env_file_path, 'a') as env_file:
-        env_file.write('\n' + "COST_DETAILS=" + str(cost_output_format)
+        env_file.write('\n' + "COST_DETAILS=" + str(cost_output_format))
         env_file.close()
 
 calculate_cost(vm_hour_rate, vm_num_int, business_days, weekend_days)
