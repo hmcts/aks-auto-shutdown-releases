9d4fb76fbb3eaf4bdb288e97ad29babaae82ae8f
Jordan Hoey <jordan.hoey@justice.gov.uk>
2024-02-22 16:24:35 +0000

    testing

--- scripts/aks/send-slack-message.sh
+++ scripts/aks/send-slack-message.sh
@@ -15,8 +15,8 @@ current_date=$(get_current_date)
 start_date="$5"
 end_date="$6"
 cost_value="£$7"
-env_input="$8"
-environment=$($env_input | jq -r '.Environment | join(",")')
+env_input=$8
+environment=($9 | jq -r '.Environment | join(",")')
 
 echo "env_input"
 echo $env_input
