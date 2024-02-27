3448c9f4f7a38d320aab92b9e20b2e9dcccb1179
Jordan Hoey <jordan.hoey@justice.gov.uk>
2024-02-27 13:45:51 +0000

    test

--- scripts/aks/send-slack-message.sh
+++ scripts/aks/send-slack-message.sh
@@ -13,14 +13,9 @@ current_date=$(get_current_date)
 environment="TEST"
 
 echo "jq output:"
+#test_env=$(echo "$new_data" | jq -r '.Environment | join(", ")')
 
-if [[ $(echo "$new_data" | jq -r '.Environment | type') == "array" ]]; then
-    test_env=$(echo "$new_data" | jq -r '.Environment | join(", ")')
-else
-    test_env=$(echo "$new_data" | jq -r '.Environment')
-fi
-
-echo $test_env
+echo $ENVIRONMENT
 
 #var_without_brackets="${environment//[\"[]/}"
 #echo "var without brackets: $var_without_brackets"
@@ -35,7 +30,7 @@ jq --arg new_url "$request_ur_link" \
    --arg start_date "$START_DATE" \
    --arg end_date "$END_DATE" \
    --arg cost_value "£$COST_DETAILS_FORMATTED" \
-   --arg environment "$test_env" \
+   --arg environment "$ENVIRONMENT" \
    '.blocks[0].text.text |= "You have a new request:\n\($new_url)" | 
     .blocks[1].fields[0].text |= "*Business Area:*\n\($business_area)" |
     .blocks[1].fields[1].text |= "*Environment:*\n\($environment)" |
