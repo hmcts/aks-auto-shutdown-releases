fb40fc521d53a287219e516855ebc7bdf0a852b6
Jordan Hoey <jordan.hoey@justice.gov.uk>
2024-02-27 14:25:34 +0000

    testing

--- scripts/aks/send-slack-message.sh
+++ scripts/aks/send-slack-message.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 source scripts/common/common-functions.sh
 
-#curl -s -X POST --data-urlencode "payload={\"channel\": \"${CHANNEL_NAME}\", \"username\": \"Plato\", \"text\": \"${MESSAGE}\", \"icon_emoji\": \":plato:\"}" ${WEBHOOK_URL}
-rm slack-payload.json
-
 # Define Bash variables
-
-# Define Bash variables
-id=$CHANGE_JIRA_ID
-request_ur_link="*<$REQUEST_URL|$id>*"
+request_ur_link="*<$REQUEST_URL|$CHANGE_JIRA_ID>*"
 current_date=$(get_current_date)
 environment_field=$(echo "$ENVIRONMENT" | sed 's/\[//; s/\]//; s/"//g')
 
@@ -16,19 +10,19 @@ environment_field=$(echo "$ENVIRONMENT" | sed 's/\[//; s/\]//; s/"//g')
 jq --arg new_url "$request_ur_link" \
    --arg business_area "$BUSINESS_AREA_ENTRY" \
    --arg environment "$environment_field" \
-   --arg current_date "$current_date" \
    --arg start_date "$START_DATE" \
    --arg end_date "$END_DATE" \
+   --arg requester "$REQUESTER" \
+   --arg current_date "$current_date" \
    --arg cost_value "£$COST_DETAILS_FORMATTED" \
-   --arg submitter "$SUBMITTER" \
    '.blocks[0].text.text |= "You have a new request:\n\($new_url)" | 
     .blocks[1].fields[0].text |= "*Business Area:*\n\($business_area)" |
     .blocks[1].fields[1].text |= "*Environment:*\n\($environment)" |
     .blocks[1].fields[2].text |= "*Start Date:*\n\($start_date)" |
     .blocks[1].fields[3].text |= "*End Date:*\n\($end_date)" |
-    .blocks[1].fields[4].text |= "*Value:*\n\($cost_value)" |
+    .blocks[1].fields[4].text |= "*Requester:*\n\($requester)" |
     .blocks[1].fields[5].text |= "*Submitted:*\n\($current_date)" |
-    .blocks[1].fields[6].text |= "*Submitter:*\n\($submitter)"' scripts/aks/message-template.json > slack-payload.json
+    .blocks[1].fields[6].text |= "*Value:*\n\($cost_value)"' scripts/aks/message-template.json > slack-payload.json
 
 MESSAGE=$(< slack-payload.json)
 
