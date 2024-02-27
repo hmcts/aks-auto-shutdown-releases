8573872547af23cd72a8062256493475d4038004
Jordan Hoey <jordan.hoey@justice.gov.uk>
2024-02-27 14:16:28 +0000

    test

--- scripts/aks/send-slack-message.sh
+++ scripts/aks/send-slack-message.sh
@@ -10,23 +10,17 @@ rm slack-payload.json
 id=$CHANGE_JIRA_ID
 request_ur_link="*<$REQUEST_URL|$id>*"
 current_date=$(get_current_date)
-
 environment_field=$(echo "$ENVIRONMENT" | sed 's/\[//; s/\]//; s/"//g')
-
-#var_without_brackets="${environment//[\"[]/}"
-#echo "var without brackets: $var_without_brackets"
-#var_without_quotes="${var_without_brackets//\"]/}"
-
-#echo "var without quotes: $var_without_quotes"
 
 # Use jq with variables
 jq --arg new_url "$request_ur_link" \
    --arg business_area "$BUSINESS_AREA_ENTRY" \
+   --arg environment "$environment_field" \
    --arg current_date "$current_date" \
    --arg start_date "$START_DATE" \
    --arg end_date "$END_DATE" \
    --arg cost_value "£$COST_DETAILS_FORMATTED" \
-   --arg environment "$environment_field" \
+   --arg submitter "$SUBMITTER" \
    '.blocks[0].text.text |= "You have a new request:\n\($new_url)" | 
     .blocks[1].fields[0].text |= "*Business Area:*\n\($business_area)" |
     .blocks[1].fields[1].text |= "*Environment:*\n\($environment)" |
@@ -34,7 +28,7 @@ jq --arg new_url "$request_ur_link" \
     .blocks[1].fields[3].text |= "*End Date:*\n\($end_date)" |
     .blocks[1].fields[4].text |= "*Value:*\n\($cost_value)" |
     .blocks[1].fields[5].text |= "*Submitted:*\n\($current_date)" |
-    .blocks[1].fields[6].text |= "*Submitter:*\n\($environment)"' scripts/aks/message-template.json > slack-payload.json
+    .blocks[1].fields[6].text |= "*Submitter:*\n\($submitter)"' scripts/aks/message-template.json > slack-payload.json
 
 MESSAGE=$(< slack-payload.json)
 
