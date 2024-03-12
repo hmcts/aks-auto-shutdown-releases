a39a0e69faead7e9a508303df967e88075adcbf9
Jordan Hoey <jordan.hoey@justice.gov.uk>
2024-03-12 15:15:28 +0000

    testing

--- scripts/aks/send-slack-message.sh
+++ scripts/aks/send-slack-message.sh
@@ -7,7 +7,7 @@ current_date=$(get_current_date)
 environment_field=$(echo "$ENVIRONMENT" | sed 's/\[//; s/\]//; s/"//g')
 
 # Use jq with variables
-jq --arg new_url "$request_ur_link" \
+jq --arg issue_url "$request_ur_link" \
    --arg business_area "$BUSINESS_AREA_ENTRY" \
    --arg environment "$environment_field" \
    --arg start_date "$START_DATE" \
@@ -15,14 +15,17 @@ jq --arg new_url "$request_ur_link" \
    --arg requester "$REQUESTER" \
    --arg current_date "$current_date" \
    --arg cost_value "£$COST_DETAILS_FORMATTED" \
-   '.blocks[0].text.text |= "You have a new request:\n\($new_url)" | 
+   --arg status "$APPROVAL_STATE" \
+   '.blocks[0].text.text |= "You have a new request:\n\($issue_url)" | 
     .blocks[1].fields[0].text |= "*Business Area:*\n\($business_area)" |
     .blocks[1].fields[1].text |= "*Environment:*\n\($environment)" |
     .blocks[1].fields[2].text |= "*Start Date:*\n\($start_date)" |
     .blocks[1].fields[3].text |= "*End Date:*\n\($end_date)" |
     .blocks[1].fields[4].text |= "*Requester:*\n\($requester)" |
     .blocks[1].fields[5].text |= "*Submitted:*\n\($current_date)" |
-    .blocks[1].fields[6].text |= "*Value:*\n\($cost_value)"' scripts/aks/message-template.json > slack-payload.json
+    .blocks[1].fields[6].text |= "*Value:*\n\($cost_value)" |
+    .blocks[1].fields[7].text |= "*Status:*\n\($status)" |
+    .blocks[] | select(.type == "actions") | .elements[].value |= "$issue_url"' scripts/aks/message-template.json > slack-payload.json
 
 MESSAGE=$(< slack-payload.json)
 
